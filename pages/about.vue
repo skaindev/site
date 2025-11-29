ccba0800654c502f2f68a1b2606693c53dd220ca
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-11-29 10:53:12 +0200

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <AboutOurTeam />
+    <AboutJoinUs />
+  </main>
+</template>
+
+<script>
+export default {
+  layout: 'page',
+  head() {
+    return {
+      title: 'About Us - Skaina',
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('../components/lib/media-query-helper.css');
+
+main {
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+
+main > * {
+  margin-top: 6rem;
+
+  @media (--viewport-medium) {
+    margin-top: 4rem;
+  }
+
+  @media (--viewport-small) {
+    margin-top: 2rem;
+  }
+}
+</style>
