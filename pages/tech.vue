3155e64ef1a201d7ccab64c83a8d5d524e7f6114
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-07 05:08:59 +0200

    Create tech.vue

---
+++ pages/tech.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <TechTechnology />
+    <TechRoadmapList />
+    <TechPublications />
+    <HomeCommunity />
+    <TechJoinUs />
+  </main>
+</template>
+
+<script>
+export default {
+  layout: 'page',
+  head() {
+    return {
+      title: 'Technology - Skaina',
+    }
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('../components/lib/media-query-helper.css');
+
+main {
+  overflow-x: hidden;
+}
+
+main > * {
+  margin-top: 6rem;
+
+  @media (--viewport-medium) {
+    margin-top: 4rem;
+  }
+
+  @media (--viewport-small) {
+    margin-top: 2rem;
+  }
+}
+</style>
