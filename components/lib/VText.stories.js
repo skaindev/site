aaaf0e0a095a48ad63230b699e76eff0ef6a7154
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-08 05:04:32 +0200

    Create VText.stories.js

---
+++ components/lib/VText.stories.js
@@ -0,0 +1,17 @@
+export const colors = () => `
+  <v-stack direction="column" gap="2rem">
+    <v-text color="primary">
+      The quick brown fox jumps over the lazy dog.
+    </v-text>
+    <v-text color="secondary">
+      The quick brown fox jumps over the lazy dog.
+    </v-text>
+    <v-text color="link">
+      The quick brown fox jumps over the lazy dog.
+    </v-text>
+  </v-stack>
+`
+
+export default {
+  title: 'VText',
+}
