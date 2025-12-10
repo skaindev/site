2a5000417979da9d5e114bb6e313888d6f376569
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-10 05:34:19 +0200

    Create NodeGrid.stories.js

---
+++ components/art/NodeGrid.stories.js
@@ -0,0 +1,9 @@
+export const base = () => `
+  <v-stack align="center" justify="center" :style="{ height: 'calc(100vh - 2rem)' }">
+    <art-node-grid width="75%" />
+  </v-stack>
+`
+
+export default {
+  title: 'art/node-grid',
+}
