20a1b44b20031aee7128e0535bd97aa1fc437ab4
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-12 22:41:33 +0200

    Create AppCopyright.vue

---
+++ components/AppCopyright.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-text font="sans" size="1rem" >
+      <a href="/terms/"
+              >
+    <v-text size="1rem" >Terms</v-text></a
+            >
+      © {{ currentYear }} Skaina.
+    </v-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCopyright',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+div {
+  color: black;
+  padding: 1.5rem;
+  text-align: center;
+  background-color: #FFF;
+  border-top: 1px solid #ccc;
+}
+</style>
