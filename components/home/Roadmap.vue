d5f740796021221f32a9dc6f6ea525d7146bcd3f
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-13 22:17:57 +0200

    Create Roadmap.vue

---
+++ components/home/Roadmap.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <v-container>
+      <v-stack>
+        <app-section-descriptor
+          title="Roadmap"
+          heading="Skaina Roadmap"
+          description="At Skaina, our relentless pursuit is to establish the forefront of payment systems. Discover our remarkable achievements and ongoing endeavors below. Inspired? Join us in shaping the future! Together, we can make Skaina the epitome of advancement in the realm of payments."
+          style="max-width: 58rem"
+        />
+      </v-stack>
+    </v-container>
+    <div class="roadmap-container">
+      <v-image
+        path="images/roadmap-desktop.png"
+        class="tablet-remove mobile-remove roadmap-desktop"
+      />
+      <div class="laptop-remove position-relative" style="text-align: center">
+        <v-image path="images/roadmap-mobile-bg.png" class="roadmap-mobile" />
+      </div>
+    </div>
+    <div class="roadmap-container">
+      <a href="/tech/" target="_blank"><v-button label="See details" label-transform="uppercase" /> </a>
+    </div>
+  </section>
+  
+</template>
+
+<script>
+export default {
+  name: 'Roadmap',
+}
+</script>
+
+<style lang="css" scoped>
+/* Imported this for getting helper classes laptop-remove and mobile-remove */
+@import url('../lib/media-query-helper.css');
+
+section {
+  display: none; /* Hidded Roadmap */
+  margin-top: 0 !important;
+  padding: 6rem 0 0;
+  color: black;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  @media (--viewport-small) {
+    padding: 3rem 0 0;
+  }
+}
+
+.roadmap-container {
+  margin-top: 6rem;
+  text-align: center;
+
+  @media (--viewport-medium) {
+    margin-top: 3rem;
+  }
+}
+
+.roadmap-desktop {
+  width: 100%;
+  max-width: 1720px;
+}
+
+.roadmap-mobile {
+  width: 100%;
+  max-width: 375px;
+}
+</style>
