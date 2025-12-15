037ef9f50b77c4d66b6cd3ca3d7cfc47300b6efa
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-15 05:28:11 +0200

    Create PageNotFound.vue

---
+++ components/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <v-container>
+      <v-stack
+        justify="space-between"
+        align="center"
+        direction="column"
+        md-justify="space-evenly"
+        class="error-content-container"
+      >
+        <v-image
+          path="images/404-illustration.png"
+          class="error-illustration"
+        />
+        <v-heading>Page Not Found!</v-heading>
+      </v-stack>
+    </v-container>
+  </section>
+</template>
+
+<style lang="postcss" scoped>
+.error-content-container {
+  height: 80vh;
+}
+
+.error-illustration {
+  width: 90%;
+  max-width: 40rem;
+}
+</style>
