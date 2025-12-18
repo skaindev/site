f113b914204d2afc9332b73c47cffde49dba3102
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-18 19:39:59 +0200

    Create Publications.vue

---
+++ components/home/Publications.vue
@@ -0,0 +1,84 @@
+<template>
+  <section>
+    <v-container class="publication-container">
+      <v-stack md-direction="column" justify="space-between" align="center">
+        <v-image
+          path="images/publications-illustration.svg"
+          class="publication-illustration"
+        />
+        <v-stack
+          direction="column"
+          gap="2rem"
+          align="start"
+          class="publication-description-container"
+        >
+          <app-section-descriptor
+            title="Skaina Blog"
+            heading="Keep up to date with Skaina and our research."
+            description="We frequently not only write about updates and partnerships at Skaina but deep dive into research, share insights through ‘Engineering at Skaina and ‘Design at Skaina series, and break down the future of Web 3."
+            class="publication-description"
+          />
+          <v-button
+            label="View Blog"
+            label-transform="uppercase"
+            class="technical-paper-btn"
+            :action="openBlog"
+          />
+        </v-stack>
+      </v-stack>
+    </v-container>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Publications',
+  methods: {
+    openBlog() {
+      window.open('https://blog.skaina.com', '_blank')
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('../lib/media-query-helper.css');
+
+section {
+  margin-top: 10rem;
+  background: #080808
+    linear-gradient(
+      180deg,
+      rgba(116, 75, 75, 0.07) 15.02%,
+      rgba(255, 255, 255, 0.07) 47.35%,
+      rgba(32, 32, 32, 0) 100%
+    );
+
+  @media (--viewport-medium) {
+    margin-top: 1rem;
+  }
+}
+
+.publication-container {
+  background: #080808;
+  padding: 4rem 0;
+}
+
+.publication-description-container {
+  width: 100%;
+  max-width: 40rem;
+}
+
+.publication-illustration {
+  width: 100%;
+  max-width: 40rem;
+}
+
+.publication-description {
+  max-width: 30rem;
+}
+
+.technical-paper-btn {
+  padding: 1rem 2rem;
+}
+</style>
