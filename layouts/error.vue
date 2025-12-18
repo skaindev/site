ca91212ea202a6584e17e9154dccc9b5b6c88b26
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-18 19:40:10 +0200

    Create error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <main v-if="error.statusCode === 404">
+      <AppHeader />
+      <PageNotFound />
+    </main>
+    <main v-else>
+      <v-heading>An error occurred</v-heading>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+}
+</script>
