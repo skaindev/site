ae62bf5f33f56fdbfe8f1b3853d8859cc0a3e17d
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-18 19:40:05 +0200

    Create VContainer.vue

---
+++ components/lib/VContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VContainer',
+}
+</script>
+
+<style lang="postcss" scoped>
+@import url('./media-query-helper.css');
+
+.container {
+  color: black;
+  max-width: 96rem;
+  width: 80%;
+  margin: 0 auto;
+  background-color: #FFF;
+
+  @media (--viewport-small) {
+    width: 90% !important;
+    margin: 0 auto;
+  }
+}
+</style>
