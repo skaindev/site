62753a14d1a3b62b1e565cbdc810fd12445568b8
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-19 19:42:47 +0200

    Update terms.vue

--- pages/terms.vue
+++ pages/terms.vue
@@ -1,47 +1,66 @@
 <template>
   <main>
-    <v-heading>Terms and conditions</v-heading>
-   <div class="terms-title">REGIONAL RESTRICTIONS</div>
-   <div class="terms-text">These Terms and Conditions constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and, 
-    Skaina and/or NIO (“we,” “us” or “our”), concerning your access to and use of the www.skaina.com website and all the subdomains 
-    (i.e. blog.skaina.com”) as well as any other media form, media channel, mobile website or mobile application related, linked, 
-    or otherwise connected thereto (collectively, the “website”).
-    You agree that by accessing the website, you have read, understood, and agree to be bound by all of these Terms and Conditions and you agree that you 
-    are responsible for compliance with any applicable local laws. If you do not agree with all of these Terms and Conditions, then you are expressly prohibited 
-    from using the website and you must discontinue its use immediately. Supplemental terms and conditions or documents that may be posted on the website from time to 
-    time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these 
-    Terms and Conditions at any time and for any reason. We will alert you about any changes by updating the “Last updated” date of these Terms and Conditions, 
-    and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Terms and Conditions to stay 
-    informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms and Conditions 
-    by your continued use of the Site after the date such revised Terms and Conditions are posted. The information provided on the website is not intended for 
-    distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. 
-    The materials on “www.skaina.com” and/or the subdomains “wallet.skaina.com” and “scan.skaina.com” website are provided on the “as is” basis. 
-    The website “www.skaina.com” and their developers and/or the contributors makes no warranties, expressed or implied, and hereby disclaims and negates
-     all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or noninfringement of 
-     intellectual property or other violation of rights. Also “www.skaina.com” does not warrant or make any representations concerning the accuracy, likely results, or 
-     reliability of the use of the materials on its websites or otherwise relating to such materials or on any websites linked to this website.</div>
+    <v-heading>Terms and Conditions</v-heading>
 
-     <div class="terms-title">RISKS OF USE</div>
-   <div class="terms-text">The risk associated with the Ethernet core agreement since Skaina (NIO) is based on the Ethernet protocol development, any failure, 
-    unpredictable functional problems, or attacks that occur in any Ethernet core protocol can cause Skaina (NIO) or our applications to stop working or can 
-    behave in an unpredictable manner. The risk associated with using third party apps, websites and/or tools that can obtains the user's login credentials or 
-    private keys will be possible to directly control the user’s funds. In order to minimize the risk, the user must protect its electronic device to prevent 
-    unauthorized access requests from accessing and accessing the device content. The risk that our applications and/or products may not meet the expectations
-     of users since our applications and/or products are currently in the development phase and may be subject to major changes before the release of the official
-      versions. Phishing and/or Theft risks associated with hackers, other organizations or even countries have the potential to attempt to interrupt our applications 
-      and/or functionality in any way, including service attacks, Sybil attacks, malware attacks, or consistent attacks. The risk of uninsured losses since the coins 
-      stored in your account is not insured like the money stored in the bank accounts or any other financial institutions. The risk of the presence and/or application 
-      failure, meaning that our websites and/or platforms may break down due to various reasons, and therefore may not be able to provide the normal, expected, services.
+    <div class="terms-title">REGIONAL RESTRICTIONS</div>
+    <div class="terms-text">
+      These Terms and Conditions constitute a legally binding agreement between you, whether personally or on behalf of an entity (“you”),
+      and Skaina and/or SKA (“we,” “us,” or “our”), governing your access to and use of the www.skaina.com website and all related subdomains
+      (including but not limited to blog.skaina.com, wallet.skaina.com, and scan.skaina.com), as well as any associated media, mobile
+      websites, or applications (collectively, the “Website”).
+
+      <br /><br />
+
+      By accessing or using the Website, you confirm that you have read, understood, and agree to be bound by these Terms and Conditions,
+      and that you are responsible for compliance with all applicable local laws. If you do not agree to these Terms, you must immediately
+      discontinue use of the Website.
+
+      <br /><br />
+
+      We reserve the right, at our sole discretion, to modify or update these Terms and Conditions at any time. Changes will be effective
+      immediately upon posting and indicated by an updated “Last updated” date. Continued use of the Website constitutes acceptance of
+      any revised Terms.
+
+      <br /><br />
+
+      The Website and its materials are provided on an “as is” and “as available” basis. Skaina makes no warranties, express or implied,
+      including but not limited to warranties of merchantability, fitness for a particular purpose, or non-infringement.
+    </div>
+
+    <div class="terms-title">RISKS OF USE</div>
+    <div class="terms-text">
+      Skaina (SKA) is built on blockchain technology and is subject to risks inherent to decentralized systems. Any failure, vulnerability,
+      bug, or attack affecting the underlying blockchain protocol may result in unexpected behavior, service interruptions, or loss of
+      functionality.
+
+      <br /><br />
+
+      Use of third-party applications, wallets, websites, or tools may expose users to security risks, including unauthorized access to
+      private keys or credentials. You are solely responsible for securing your devices and safeguarding your private keys.
+
+      <br /><br />
+
+      Skaina products and services may be in active development and subject to change. Features may be modified, delayed, or removed,
+      and functionality may not meet user expectations.
+
+      <br /><br />
+
+      Cryptocurrency assets are not insured. Losses due to hacks, exploits, software errors, or network failures are not recoverable
+      and Skaina bears no responsibility for such losses.
     </div>
 
     <div class="terms-title">DISCLAIMER</div>
-   <div class="terms-text">We reserve the right to change, add, suspend, cancel, remove or otherwise modify the services and information offered on this website at any time without prior notice.
- 
-
+    <div class="terms-text">
+      Skaina reserves the right to modify, suspend, or discontinue any part of the Website or its services at any time without prior notice.
+      We do not guarantee the accuracy, reliability, or availability of any content or services provided.
     </div>
-    <div class="terms-text">Last updated: July 01, 2023</div>
+
+    <div class="terms-text">
+      Last updated: December 18, 2025
+    </div>
   </main>
 </template>
+
 <style>
 .terms-title {
   font-size: 1.375rem;
