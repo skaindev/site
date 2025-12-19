7e90c1439c91e0f5a0f90d3fa2697968e55c44e2
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-19 19:44:39 +0200

    Update privacy.vue

---
+++ pages/privacy.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <v-heading>Privacy Policy</v-heading>
+
+    <div class="terms-title">INTRODUCTION</div>
+    <div class="terms-text">
+      This Privacy Policy describes how Skaina and/or SKA (“we,” “us,” or “our”) collects, uses, stores, and protects your information
+      when you access or use the www.skaina.com website and its subdomains, including but not limited to blog.skaina.com,
+      wallet.skaina.com, and scan.skaina.com (collectively, the “Website”).
+    </div>
+
+    <div class="terms-title">INFORMATION WE COLLECT</div>
+    <div class="terms-text">
+      Skaina does not knowingly collect personal identification information such as names, addresses, or government-issued IDs
+      unless explicitly provided by you.
+
+      <br /><br />
+    
+    </div>
+  </main>
+</template>
