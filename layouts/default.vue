e626ef90b45a3f788654e4c70fd55ba69b1c5f02
MrTizu <204244194+skaindev@users.noreply.github.com>
2025-12-22 04:14:37 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,22 @@
 <template>
-  <Nuxt />
+  <div>
+    <AppHeader />
+    <Nuxt />
+    <AppFooter />
+    <AppLegalConsent />
+  </div>
 </template>
+
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
+import AppLegalConsent from '@/components/AppLegalConsent.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    AppLegalConsent
+  }
+}
+</script>
