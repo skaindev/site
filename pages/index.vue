cb9103d821ec29f45acaffbfdad5304ba0e7cc8e
MrTizu <204244194+skaindev@users.noreply.github.com>
2026-01-09 06:54:03 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,33 @@
     <AppLegalConsent />
 
     <div id="app-shell">
+      <AppHeader />
       <Nuxt />
+      <AppFooter />
     </div>
   </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 import AppLegalConsent from '@/components/AppLegalConsent.vue'
 
 export default {
   components: {
+    AppHeader,
+    AppFooter,
     AppLegalConsent
   }
 }
 </script>
+
+<style>
+#layout-root {
+  position: static;
+}
+
+#app-shell {
+  position: relative;
+}
+</style>
